af8cf54a2cc19cd520f94016ce9204ab5b309b21
manivannancsmca <manivannancsmca@gmail.com>
2026-08-22 15:52:57 +0530

    ai-agent-service appliction worked

---
+++ ai-agent-service/src/main/java/com/enterprise/aiagent/AiAgentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.aiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAgentServiceApplication.class, args);
+	}
+
+}
---
+++ ai-agent-service/src/test/java/com/enterprise/aiagent/AiAgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.aiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
